cabd949a95ca87bbb2f172c539669c27c22c4271
letoem <102681999+letoem@users.noreply.github.com>
2023-09-11 15:42:45 +0200

    Login Page ui - done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,20 @@
 	import * as Card from "$lib/components/ui/card";
 	import { Label } from "$lib/components/ui/label";
 	import { Input } from "$lib/components/ui/input";
+    
+    let mail: boolean = false;
+    let email: string;
+
+    function handleEvent() {
+        console.log(email)
+        mail = true;
+    }
 </script>
 
 <section class="h-screen w-screen flex justify-center items-center">
 
-        <div id="card" class="flex justify-center items-center w-80 sm:w-96 max-w-sm">
+    <div id="card" class="flex justify-center items-center w-80 sm:w-96 max-w-sm">
+        {#if !mail}
             <Card.Root class="sm:p-2">
                 <Card.Header class="space-y-1">
                     <Card.Title class="text-2xl">Login</Card.Title>
@@ -23,13 +32,37 @@
                     </div>
                     <div class="grid gap-2">
                         <Label for="email">E-Mail Adresse</Label>
-                        <Input id="email" type="email" placeholder="vorname.name@schule-muenchenstein.ch" />
+                        <Input id="email" type="email" placeholder="vorname.name@schule-muenchenstein.ch" bind:value={email}/>
                     </div>
                 </Card.Content>
                 <Card.Footer>
-                    <Button class="w-full">Link senden</Button>
+                    <Button on:click={handleEvent} class="w-full">Link senden</Button>
                 </Card.Footer>
             </Card.Root>
-        </div>
+        {:else}
+            <Card.Root class="sm:p-2">
+                <Card.Header class="space-y-1">
+                    <Card.Title class="text-2xl">E-Mail wurde verschickt!</Card.Title>
+                    <Card.Description>
+                        Bitte überprüfe dein Postfach und klicke auf den Link, um dich einzuloggen.
+                    </Card.Description>
+                </Card.Header>
+                <Card.Content class="grid gap-4">
+                    <div class="relative">
+                        <div class="absolute inset-0 flex items-center">
+                            <span class="w-full border-t" />
+                        </div>
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="email">E-Mail Adresse</Label>
+                        <Input disabled id="email" type="email" placeholder="vorname.name@schule-muenchenstein.ch"/>
+                    </div>
+                </Card.Content>
+                <Card.Footer>
+                    <Button variant="outline" disabled class="w-full">Link senden</Button>
+                </Card.Footer>
+            </Card.Root>
+        {/if}
+    </div>
 
 </section>
